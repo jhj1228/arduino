045ac31aac3af583cabc09c3b651ef77c2a8ae1c
정희정 <43434644+jhj1228@users.noreply.github.com>
2023-10-06 13:13:08 +0900

    Create 5번프로세싱.java

---
+++ 5번프로세싱.java
@@ -0,0 +1,38 @@
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(piezoPin, OUTPUT);
+  
+  Serial.begin(9600);
+  
+  tone(4, 523, 500);
+  delay(500 * 1.3);
+  tone(4, 587, 500);
+  delay(500 * 1.3);
+  tone(4, 659, 500);
+  delay(500 * 1.3);
+  tone(4, 698, 500);
+  delay(500 * 1.3);
+  tone(4, 783, 500);
+  delay(500 * 1.3);
+  tone(4, 880, 500);
+  delay(500 * 1.3);
+  tone(4, 987, 500);
+  delay(500 * 1.3);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readString();
+    command.trim();
+
+    if (command == "on") {
+      digitalWrite(ledPin, HIGH);
+    } 
+    else if (command == "off") {
+      digitalWrite(ledPin, LOW);
+    }
+    else if (command.toInt() >= 1 && command.toInt() <=8) { 
+      playTone(command.toInt());
+    }
+  }
+}
